6c89e9d2261b86b255afbf62b1ad361c1900d296
NataliaSheridan <90013086+NataliaSheridan@users.noreply.github.com>
2021-10-19 13:20:09 -0600

    fixed spelling mistake

--- films/starwars-films.js
+++ films/starwars-films.js
@@ -1,23 +1,24 @@
 import { films} from '../data/films.js'
 
-let filmlist = document.querySelector('#filmlist')
-
+let filmList = document.getElementById('filmlist')
+console.log(filmlist)
 let titlelist = document.createElement('ol')
 
-filmList.appendChild(titleList)
 
-figImg.src = 'https://starwars-visualguide.com/assets/img/films/1.jpg'
+//filmList.appendChild(titleList)
+
 let figCaption = document.createElement('figcaption')
 
 for(let i = 0; i < films.length; i++){
     let figure = document.createElement('figure')
-let figure = document.createElement('img')
+    let image = document.createElement('img')
     let titleItem = document.createElement('li')
     titleItem.textContent = films[i]
-figCaption.textContent = films[i].title
+    figCaption.textContent = films[i].title
+    image.src = 'https://starwars-visualguide.com/assets/img/films/1.jpg'
 
-figure.appendChild(figImg)
-figure.appendChild(figCaption)
+    figure.appendChild(image)
+    figure.appendChild(figCaption)
 
-filmList.appendChild(figure)
+    filmList.appendChild(figure)
 }
