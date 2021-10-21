3f51d6a5bc3082a5e9b6681027baa45b4421cf67
NataliaSheridan <90013086+NataliaSheridan@users.noreply.github.com>
2021-10-21 13:12:58 -0600

    fixed code for images

--- characters/sw-chars.js
+++ characters/sw-chars.js
@@ -4,32 +4,36 @@ const main = document.querySelector('#main')
 
 const maleCharacters = people.filter( person => person.gender === 'male')
 console.log(maleCharacters.length)
-const femaleCharecters = people.filter( person => person.gender === 'female')
+const femaleCharacters = people.filter( person => person.gender === 'female')
 console.log(femaleCharacters.length)
 
-const otherCharacter = people.filter(person => {
+const otherCharacters = people.filter(person => {
     if(person.gender ==='n/a' || person.gender === 'hermaprhodite') { return person}
 })
-people.forEach((element) => {
-    if (index !==17) {
+console.log(otherCharacters)
+
+femaleCharacters.forEach((element) => {
 const personFig = document.createElement('figure')
-const personImg = document.createElement('img')
-personImg.src = `https://starwars-visualguide.com/assets/img/characters/${
-    index + 1
-  }.jpg`
+const personImg = document.createElement ('img')
+let charNum = getLastNumber(element.url)
+personImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+const personCaption = document.createElement('figcaption')
+personCaption.textContent = element.name
+
 personCaption.textContent = element.name
 personFig.appendChild(personImg)
 personFig.appendChild(personCaption)
 
 main.appendChild(personFig)
 getLastNumber(element.url)
-    }
+    
 })
 function getLastNumber(url) {
-    let end = url.lastIndexof('/')
+    let end = url.lastIndexOf('/')
     let start = end -2
     console.log(url)
-    if (url.charAt(start) === "/") {
+    if (url.charAt(start) === '/') {
         start++
     }
+    return url.slice(start, end)
 }
