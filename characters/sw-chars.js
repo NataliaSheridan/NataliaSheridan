68f93cecd9e4a6453364869ccf5484b2fb0a8275
NataliaSheridan <90013086+NataliaSheridan@users.noreply.github.com>
2021-10-21 12:40:05 -0600

    added some code to java added the get last number function

--- characters/sw-chars.js
+++ characters/sw-chars.js
@@ -1,11 +1,35 @@
 import { people } from '../data/people.js'
+
 const main = document.querySelector('#main')
 
+const maleCharacters = people.filter( person => person.gender === 'male')
+console.log(maleCharacters.length)
+const femaleCharecters = people.filter( person => person.gender === 'female')
+console.log(femaleCharacters.length)
+
+const otherCharacter = people.filter(person => {
+    if(person.gender ==='n/a' || person.gender === 'hermaprhodite') { return person}
+})
+people.forEach((element) => {
+    if (index !==17) {
 const personFig = document.createElement('figure')
 const personImg = document.createElement('img')
-personImg.src = 'http://starwars-visualguide.com/assets/img/characters/1.jpg'
-personCaption.textContent = 'person name goes here'
+personImg.src = `https://starwars-visualguide.com/assets/img/characters/${
+    index + 1
+  }.jpg`
+personCaption.textContent = element.name
 personFig.appendChild(personImg)
 personFig.appendChild(personCaption)
 
 main.appendChild(personFig)
+getLastNumber(element.url)
+    }
+})
+function getLastNumber(url) {
+    let end = url.lastIndexof('/')
+    let start = end -2
+    console.log(url)
+    if (url.charAt(start) === "/") {
+        start++
+    }
+}
