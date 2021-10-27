13cac90e8ab6c789e8e1f7ba73a65c444aa85f29
NataliaSheridan <90013086+NataliaSheridan@users.noreply.github.com>
2021-10-27 14:37:05 -0600

    fixed coding on sw chars

--- characters/sw-chars.js
+++ characters/sw-chars.js
@@ -1,34 +1,40 @@
 import { people } from '../data/people.js'
+import { getLastNumber } from '..utils/index.js'
 
 const main = document.querySelector('#main')
 
 const mainHeader = document.createElement('header')
 document.body.insertBefore(mainHeader, main)
 
+const allButton = document.createElement('button')
+allButton.textContent = "All Characters"
+allButton.addEventListener('click,( => populationDOM(people)')
+mainHeader.appendChild(allButton)
+
 const maleButton = document.createElement('button')
-maleButton.textContent = "male charecters"
-mainHeader.appendChild(maleButton)
-
-maleButton.addEventListener('click',()=> populateDOM(maleCharacters))
-
+maleButton.textContent = "Male Characters"
+maleButton.addEventListener('click,( => populationDOM(maleCharacters)')
 mainHeader.appendChild(maleButton)
 
 const femaleButton = document.createElement('button')
-femaleButton.textContent = "female charecters"
+femaleButton.textContent = "female characters"
 femaleButton.addEventListener('click', ()=>populateDOM(femaleCharacters))
 mainHeader.appendChild(femaleButton)
 
 const othersButton = document.createElement('button')
-otherButton.textContent = "other charecters"
+otherButton.textContent = "other characters"
 othersButton.addEventListener('click', ()=>populateDOM(otherCharacters))
-mainHeader.appendChild(otherButton)
-const maleCharacters = people.filter( person => person.gender === 'male')
+mainHeader.appendChild(othersButton)
+
+const maleCharacters = people.filter((person)=> person.gender === 'male')
 console.log(maleCharacters.length)
-const femaleCharacters = people.filter( person => person.gender === 'female')
+const femaleCharacters = people.filter((person) => person.gender === 'female')
 console.log(femaleCharacters.length)
 
-const otherCharacters = people.filter(person => {
-    if(person.gender ==='n/a' || person.gender === 'hermaprhodite'|| person.gender === 'none') { return person}
+const otherCharacters = people.filter((person) => {
+    if(
+        person.gender ==='n/a' || person.gender === 'hermaprhodite'|| person.gender === 'none') { return person
+        }
 })
 console.log(otherCharacters)
 
@@ -45,7 +51,6 @@ personImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNu
 const personCaption = document.createElement('figcaption')
 personCaption.textContent = element.name
 
-personCaption.textContent = element.name
 personFig.appendChild(personImg)
 personFig.appendChild(personCaption)
 
