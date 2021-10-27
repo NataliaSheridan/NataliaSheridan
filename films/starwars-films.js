3748d088b7ee7a6ca4e4a89fe370bdffba546f3e
NataliaSheridan <90013086+NataliaSheridan@users.noreply.github.com>
2021-10-27 14:47:07 -0600

    added in code to index.js

--- films/starwars-films.js
+++ films/starwars-films.js
@@ -1,6 +1,6 @@
 import { films} from '../data/films.js'
 import { getLastNumber } from'../utils/index.js'
-let filmList = document.getElementById('filmlist')
+let filmList = document.querySelector('filmlist')
 console.log('filmlist')
 let titlelist = document.createElement('ol')
 
@@ -11,14 +11,15 @@ let figCaption = document.createElement('figcaption')
 
 for(let i = 0; i < films.length; i++){
     let figure = document.createElement('figure')
-    let image = document.createElement('img')
-    let titleItem = document.createElement('li')
-    titleItem.textContent = films[i]
-    figCaption.textContent = foundFilm.title
-    image.src = 'https://starwars-visualguide.com/assets/img/films/1.jpg'
+    let figImg = document.createElement('img')
+    figImg.src=  `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
+    let figCaption = document.createElement('figcaption')
+
     const foundFilm = films.find(film => getLastNumber(film.url)=== (i + 1).toString())
 
-    figure.appendChild(image)
+    figCaption.textContent = foundFilm.title
+
+    figure.appendChild(figImg)
     figure.appendChild(figCaption)
 
     filmList.appendChild(figure)
