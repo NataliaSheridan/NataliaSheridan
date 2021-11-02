28476acff7d5aba9ccc94d618c7e22724300eec2
NataliaSheridan <90013086+NataliaSheridan@users.noreply.github.com>
2021-11-02 12:40:57 -0600

    added modal images and style to my css

--- starships/sw-starships.js
+++ starships/sw-starships.js
@@ -4,6 +4,13 @@ const nav = document.querySelector('.nav')
 const navList = document.querySelector('.navList')
 const shipView = document.querySelector('.shipViewer')
 
+const modal = document.querySelector('.modal')
+const closeButton = document.querySelector('.modal-close')
+const modalBackground = document.querySelector('.modal-background')
+
+closeButton.addEventListener('click',()=>modal.classList.toggle('is-active'))
+modalBackground.addEventListener('click',()=>modal.classList.toggle('is-active'))
+
 function populateNav(){
     starships.forEach(starship => {
         let anchorWrap = document.createElement('a')
@@ -26,8 +33,16 @@ function populateShipView(shipData) {
     removeChildren(shipView)
     console.log(`You clicked on ${shipData.name}`)
     let shipImage = document.createElement('img')
+    let shipName = document.createElement('div')
+    shipName.className="shipName"
+    shipName.textContent= 'shipName'
     let shipNum = getLastNumber (shipData.url)
     shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
     shipView.appendChild(shipImage)
-    
+    shipImage.addEventListener('error',()=>{
+        shipName.textContent=""
+        shipImage.hidden=true
+        modal.classList.toggle('is-active')
+    })
+    shipView.appendChild(shipImage)
 }
